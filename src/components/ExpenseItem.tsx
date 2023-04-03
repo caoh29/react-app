ada9d14ae39d53f2f59ea2bc797058dd8617540d
Camilo Ordoñez <cronox20@gmail.com>
2023-04-03 12:26:17 -0500

    ready to go without update title btn

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
@@ -10,25 +8,15 @@ interface ExpenseItemProps {
 }
 
 function ExpenseItem(props: ExpenseItemProps): JSX.Element {
-
-    const [title, setTitle] = useState(props.title);
-
-    function clickHandler () {
-        setTitle('Updated!');
-    }
-
     return (
         <Card className="container bg-secondary my-2">
             <div className="d-flex align-items-center">
                 <ExpenseDate date={props.date} />
                 <div className="col-6">
-                    <h2 className="text-left">{title}</h2>
+                    <h2 className="text-left">{props.title}</h2>
                 </div>
-                <div className="col">
+                <div className="col d-flex justify-content-end">
                     <h2 className="btn btn-primary btn-outline-light">${props.amount}</h2>
-                </div>
-                <div className="col">
-                    <button onClick={clickHandler}>Update Title</button>
                 </div>
             </div>
         </Card>
